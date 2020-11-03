053e0852aff95d62053f125deca673097d75d5e0
Philipp // Desktop <P.Killermann@gmx.net>
2020-11-03 15:21:43 +0100

    Implemented template for classification.

--- train_classifier.py
+++ train_classifier.py
@@ -25,55 +25,10 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, H
 from sklearn.multioutput import MultiOutputClassifier
 
 
-def accuracy(model, yT, yP):
-    '''
-    Calculate accuracy of handed in data-sets
-    '''    
-    labels = np.unique(yP)        
-    
-    print('Accuracy: ', (yT==yP).mean())
-
-
-def report(dataframe, pipeline):
-    '''
-    Classification report for all the columns
-    '''
-    
-    # make prediction
-    Y_pred = pipeline.predict(X_test)    
-    
-    # list of all column names
-    targets = [item for item in dataframe.drop(columns=ignore).columns]
-        
-    # iterate through all columns
-    for index in range(0, len(targets)):    
-        
-        print(classification_report(
-            Y_test[:, index], 
-            Y_pred[:, index], 
-            target_names=[targets[index]])
-         )  
-
-
-def tokenize(text):
-    '''
-    Tokenize engine for the feature generation
-    '''
-
-    tokens = word_tokenize(text)
-    lemmatizer = WordNetLemmatizer()
-
-    clean_tokens = []
-
-    for tok in tokens:
-        clean_tokens.append(lemmatizer.lemmatize(tok).lower().strip())
-
-    return clean_tokens
-
-def main():
+def load_data(database_filepath):
 
     # creating engine for sql
-    engine = create_engine('sqlite:///' + sys.argv[1])
+    engine = create_engine('sqlite:///' + database_filepath)
 
     # load data from database
     df = pd.read_sql_table('InsertTableName', engine)
@@ -84,6 +39,27 @@ def main():
     # output values for model
     ignore = ['id', 'message', 'original', 'genre']
     Y = df.drop(columns=ignore).values
+
+    # list of all column names
+    category_names = [item for item in dataframe.drop(columns=ignore).columns]
+
+    return X, Y, category_names
+
+
+def tokenize(text):
+    
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+
+    for tok in tokens:
+        clean_tokens.append(lemmatizer.lemmatize(tok).lower().strip())
+
+    return clean_tokens
+
+
+def build_model():
 
     # creates new ml pipeline
     pipeline = Pipeline([
@@ -112,9 +88,6 @@ def main():
         )))
     ])
 
-    # split data set into train and test sets
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y)    
-
     # parameters for grid search optimization
     parameters = {        
         'features__text_count__vectorizations__max_df': (0.5, 0.75, 1.0),        
@@ -124,11 +97,60 @@ def main():
     # specify all parameters for grid search.
     model = GridSearchCV(pipeline, param_grid=parameters)   
 
-    # train and optimize the model parameters
-    model.fit(X_train, Y_train)     
+    return model
 
+
+def evaluate_model(model, X_test, Y_test, category_names):
+     
+    # make prediction
+    Y_pred = model.predict(X_test)    
+        
+    # iterate through all columns
+    for index in range(0, len(category_names)):    
+        
+        print(classification_report(
+            Y_test[:, index], 
+            Y_pred[:, index], 
+            target_names=[category_names[index]])
+         )  
+
+
+def save_model(model, model_filepath):
+    
     # save the trained pipeline into pickles.
-    pickle.dump(model, open(sys.argv[2], 'wb'))
-   
-if __name__ == "__main__":
+    pickle.dump(model, open(model_filepath, 'wb'))
+
+
+def main():
+
+    if len(sys.argv) == 3:
+
+        database_filepath, model_filepath = sys.argv[1:]
+
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
     main()
